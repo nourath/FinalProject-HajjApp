2f6fff749e2afd4f7a78500627fca317d16d722c
NouraAlthenayan <noura.yarob@gmail.com>
2021-01-05 17:55:57 +0300

    Added Firebase to project + did user's authentication and data retrieval

--- FinalProject-HajjApp/FinalProject-HajjApp/HajjInfoViewController.swift
+++ FinalProject-HajjApp/FinalProject-HajjApp/HajjInfoViewController.swift
@@ -6,6 +6,9 @@
 //
 
 import UIKit
+import Firebase
+import SDWebImage
+
 
 class HajjInfoViewController: UIViewController {
     
@@ -23,32 +26,88 @@ class HajjInfoViewController: UIViewController {
     
     @IBOutlet var hajjNationality: UILabel!
     
-    @IBOutlet var hajjLanguage: UIView!
+    @IBOutlet var hajjLanguage: UILabel!
     
     @IBOutlet var hajjAge: UILabel!
     @IBOutlet var hajjGender: UILabel!
     
     @IBOutlet var hajjBloodType: UILabel!
     
-    @IBOutlet var compaignName: UILabel!
+    @IBOutlet var campaignName: UILabel!
     
-    @IBOutlet var compaignLocation: UILabel!
+    @IBOutlet var campaignLocation: UILabel!
     
     @IBOutlet var hajjPhoneNumber: UILabel!
     
-    @IBOutlet var hajjOtherLanguge: UILabel!
+    @IBOutlet var hajjOtherLanguges: UILabel!
     
     @IBOutlet var hajjChronicDiseases: UILabel!
     
-    
-    
+
     @IBOutlet var barcodePressed: UIImageView!
+    
+    
+    var hajj: Hajj?
     
     override func viewDidLoad() {
         healthConditionView.roundCorners(corners: [.topLeft,.topRight], radius: 30)
         
         additionalInfoView.roundCorners(corners: [.topLeft,.topRight], radius: 30)
         campaignView.roundCorners(corners: [.topLeft,.topRight], radius: 30)
+        fetchUserName()
+        
+//        FirebaseConstants.picStorageRef.downloadURL(completion: { (url, error) in
+//        print("Image URL: \((url?.absoluteString)!)")
+//
+////        self.writeDatabaseCustomer(imageUrl: (url?.absoluteString)!)
+//        })
+    }
+    
+    
+    func fetchUserName() {
+        
+        if let userId = FirebaseConstants.userID?.uid {
+            FirebaseConstants.users.getDocuments { (snapshot, err) in
+                if let err = err {
+                    print("Error getting user's name: \(err)")
+                } else {
+                    
+                    if let currentUserDoc = snapshot?.documents.first(where: { ($0["uid"] as? String) == userId }) {
+                        let getName = currentUserDoc["fullName"] as! String
+                        let getAge = currentUserDoc["age"] as! Int
+                        let getBloodType = currentUserDoc["bloodType"] as! String
+                        let getCampaign = currentUserDoc["campaign"] as! String
+                        let getChronicDisseases = currentUserDoc["chronicDiseases"] as! String
+                      //  let getcurrentLocation = currentUserDoc["fullName"] as! String
+                        let getGender = currentUserDoc["gender"] as! String
+                        let getMainLanguage = currentUserDoc["mainLanguage"] as! String
+                        let getNationality = currentUserDoc["nationality"] as! String
+                        let getOtherLanguages = currentUserDoc["otherLanguages"] as! String
+                        let getPermitNumber = currentUserDoc["permitNumber"] as! String
+                        let getPhoneNumber = currentUserDoc["phoneNumber"] as! Int
+                        let getPicURL = currentUserDoc["picture"]
+                        
+                        self.hajjName.text = getName
+                        self.hajjPermitNumber.text = getPermitNumber
+                        self.hajjNationality.text = getNationality
+                        self.hajjLanguage.text = getMainLanguage
+                        self.hajjAge.text = String(getAge)
+                        self.hajjPermitNumber.text = getPermitNumber
+                        self.hajjGender.text = getGender
+                        self.hajjBloodType.text = getBloodType
+                        self.campaignName.text = getCampaign
+                       // self.campaignLocation.text =
+                        self.hajjPhoneNumber.text = String(getPhoneNumber)
+                        self.hajjOtherLanguges.text = getOtherLanguages
+                        self.hajjChronicDiseases.text = getChronicDisseases
+//                        self.hajjPic.sd_setImage(with: URL(string: "https://\(getPicURL)" ), completed: nil)
+
+
+                        print(getName)
+                    }
+                }
+            }
+        }
     }
     
 }
--- FinalProject-HajjApp/FinalProject-HajjApp/HajjLoginViewController.swift
+++ FinalProject-HajjApp/FinalProject-HajjApp/HajjLoginViewController.swift
@@ -20,9 +20,70 @@ class HajjLoginViewController: UIViewController {
     
     
     @IBAction func loginButtonTapped(_ sender: DesignableButton) {
-        transitionToMainTabScreen()
+        print(validateFields())
     }
     
+    // MARK: - User Authentication Steps
+    /*
+     Fields authentication and validation steps:
+     1. Checking if fields are filled or empty.
+        1.1 Call function `validateFields()`
+     
+     2. Checking if user credentials are matching.
+        2.1 Call function `firebaseAuth()`
+        2.2 If all conditions were true, then sign the user in.
+     
+     3. Transition to Home Screen.
+        3.1 Call function `transtionToHome()`
+     */
+    
+    func validateFields() -> String {
+        
+        //check if filled or empty
+        if emailTextField.text?.trimmingCharacters(in: .whitespacesAndNewlines) == "" ||
+            passwordTextField.text?.trimmingCharacters(in: .whitespacesAndNewlines) == "" {
+            
+            // create the alert
+            let alert = UIAlertController(title: "Empty Fields", message: "Please fill out all the fields.", preferredStyle: UIAlertController.Style.alert)
+            // add an action (button)
+            alert.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil))
+            // show the alert
+            self.present(alert, animated: true, completion: nil)
+            
+            return "Please fill out all the fields."
+            
+        } else {
+                        
+            firebaseAuth()
+            return ""
+        }
+    }
+
+    func firebaseAuth() {
+        
+        //clean version of text fields
+        let email = emailTextField!.text!.trimmingCharacters(in: .whitespacesAndNewlines)
+        let password = passwordTextField.text!.trimmingCharacters(in: .whitespacesAndNewlines)
+
+        //Signing in
+        FirebaseConstants.firebaseAuth.signIn(withEmail: email, password: password) { (result, error) in
+
+            if error != nil {
+                print(error!.localizedDescription)
+                // create the alert
+                let alert = UIAlertController(title: "Error", message: "\(error!.localizedDescription)", preferredStyle: UIAlertController.Style.alert)
+                // add an action (button)
+                alert.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil))
+                // show the alert
+                self.present(alert, animated: true, completion: nil)
+        }
+            else {
+                self.transitionToMainTabScreen()
+            }
+        }
+
+    }
+
     func transitionToMainTabScreen() {
         
         let storyboard = UIStoryboard(name: "TabBar", bundle: nil)
